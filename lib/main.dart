bd1c22479de091c03ac1b67bd59c655c41f4a144
Nyawira Muturi <albinamuturi@gmail.com>
2020-09-24 15:37:25 +0300

    add email section

--- lib/main.dart
+++ lib/main.dart
@@ -35,14 +35,50 @@ class MyApp extends StatelessWidget {
                 ),
               ),
               Container (
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                padding: EdgeInsets.all(10.0),
                 child: Row(
                   children: <Widget>[
                     Icon(
                       Icons.phone,
+                      color: Colors.teal,
                     ),
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Text (
+                        '+2547 23060606',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 20.0,
+                        fontFamily: 'Abel'
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container (
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                padding: EdgeInsets.all(10.0),
+                child: Row(
+                  children: <Widget>[
                     Icon(
                       Icons.email,
-                    )
+                      color: Colors.teal,
+                    ),
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Text (
+                      'albinamuturi@gmail.com',
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 20.0,
+                          fontFamily: 'Abel'
+                      ),
+                    ),
                   ],
                 ),
               ),
